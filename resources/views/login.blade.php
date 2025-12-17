80ee94bffae9da940fa25ce38c69cd186469d8b6
LAPTOP-J9U9GBB8\curin <curini>
2025-12-17 13:21:49 +0100

    fix view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,8 @@
 
-@if (data_get(config('services'), 'github.client_id'))
+@if (data_get(config('password'), 'github.client_id'))
     <div class="container bg-white">
         <div class="flex justify-center">
-            <a class="gap-2 rounded-full px-5 py-2 text-sm leading-5 text-white bg-[#1b1b18] hover\:bg-gray-100"
+            <a class="gap-2 rounded-full p-2 text-sm leading-5 text-white bg-zinc-500 hover:bg-zinc-700"
             href="{{ route('socialite.redirect', ['driver' => 'github']) }}">
                 <img class="inline-block" src="https://d1mj7kpaxms69g.cloudfront.net/assets/github_logo_light-5473d144f61893d30a58ba3f4fee4ece8d4b6425bb1f00d7f856b3d65d04ab9d.svg">
                 {{ __('Sign in with GitHub') }}
