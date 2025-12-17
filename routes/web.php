c0c93eef45b400e79e3ae704c5a6f5c3721a97a9
LAPTOP-J9U9GBB8\curin <curini>
2025-12-17 12:32:32 +0100

    fix namespace

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\SocialiteController;
+use Curini\Password\Http\Controllers\SocialiteController;
 
 
 Route::get('/app-auth/redirect', [SocialiteController::class, 'redirect'])->name('socialite.redirect');
