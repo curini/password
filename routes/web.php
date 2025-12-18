21fef9a2893e6901100f9711c82b7ddd7c7ff88a
LAPTOP-J9U9GBB8\curin <curini>
2025-12-18 12:17:12 +0100

    add middleware web

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use Curini\Password\Http\Controllers\SocialiteController;
 
-
-Route::get('/app-auth/redirect', [SocialiteController::class, 'redirect'])->name('socialite.redirect');
-Route::get('/app-auth/callback', [SocialiteController::class, 'callback'])->name('socialite.callback');
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/app-auth/redirect', [SocialiteController::class, 'redirect'])->name('socialite.redirect');
+    Route::get('/app-auth/callback', [SocialiteController::class, 'callback'])->name('socialite.callback');
+});
